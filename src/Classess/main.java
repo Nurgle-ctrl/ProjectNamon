1ed63a14b0cca762cb71f5a0ba377745c1192800
Nurgle-ctrl <franzpeterjosephbraga@gmail.com>
2025-03-08 00:58:26 +0800

    version -8  added "Logics, Access the database baseline categorical etc..."

--- src/Classess/main.java
+++ src/Classess/main.java
@@ -4,17 +4,43 @@
  */
 package Classess;
 
+import java.awt.event.ItemEvent;
+
 /**
  *
  * @author Acer
  */
 public class main extends javax.swing.JFrame {
-
+    private helper helpus;
+    private String tablename = "thetable";
+    private String category;
+    private String where;
+    private String postlimit;
+    private String thelimit;
+    private int index;
+    private void searchlimiter(){
+         if(!limit.getSelectedItem().toString().equals("Limit")){
+            limiter.setEnabled(false);
+            limiter.setVisible(false);
+        } else {
+             limiter.setEnabled(true);
+            limiter.setVisible(true);           
+        }
+        if(limit.getItemAt(1).equals("All")){
+            limiter.setEnabled(true);
+            limiter.setVisible(true);                
+        }         
+        limiter.revalidate();
+        limiter.repaint();       
+    }    
     /**
      * Creates new form main
      */
     public main() {
         initComponents();
+        helpus = new helper();
+        helpus.connector();
+        searchlimiter();
     }
 
     /**
@@ -27,6 +53,15 @@ public class main extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jTextField1 = new javax.swing.JTextField();
+        limit = new javax.swing.JComboBox<>();
+        limiter = new javax.swing.JTextField();
+        columnnames = new javax.swing.JComboBox<>();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        thetable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -35,15 +70,108 @@ public class main extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(255, 255, 51));
         jPanel1.setPreferredSize(new java.awt.Dimension(926, 574));
 
+        jLabel1.setText("Ano nadi?");
+
+        jLabel2.setText("ekis nadi");
+        jLabel2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel2MouseClicked(evt);
+            }
+        });
+
+        jButton1.setText("Display");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jTextField1.setHorizontalAlignment(javax.swing.JTextField.TRAILING);
+        jTextField1.setText("Condition");
+        jTextField1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTextField1MouseClicked(evt);
+            }
+        });
+
+        limit.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "All", "Limit" }));
+        limit.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                limitItemStateChanged(evt);
+            }
+        });
+        limit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                limitActionPerformed(evt);
+            }
+        });
+
+        limiter.setEditable(false);
+        limiter.setText("ninja");
+        limiter.setEnabled(false);
+        limiter.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                limiterMouseClicked(evt);
+            }
+        });
+
+        columnnames.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Posted", "DatePosted", "DocNumber", "BusinessCode", "LocationCode", "ModuleCode", "AccountCode", "NormalBalance" }));
+        columnnames.setToolTipText("");
+
+        thetable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"
+            }
+        ));
+        jScrollPane1.setViewportView(thetable);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 930, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 818, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 870, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(columnnames, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 233, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(limit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(limiter, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 570, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1))
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton1)
+                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(limit, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(limiter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(columnnames, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(28, 28, 28)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 442, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(37, Short.MAX_VALUE))
         );
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 930, 570));
@@ -51,6 +179,59 @@ public class main extends javax.swing.JFrame {
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_jLabel2MouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        category = columnnames.getSelectedItem().toString();
+        index = 1 + columnnames.getSelectedIndex();
+        where = jTextField1.getText();
+        postlimit = limit.getSelectedItem().toString();
+        thelimit = limiter.getText();
+        System.out.println(index);
+        helpus.displaythis(category, index, where, postlimit.equals("All") ? "" : thelimit, "", tablename);
+
+            
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void limitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_limitActionPerformed
+        // TODO add your handling code here:
+        searchlimiter();
+    }//GEN-LAST:event_limitActionPerformed
+   
+    private void limiterMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_limiterMouseClicked
+        // TODO add your handling code here:
+        limiter.setText("");
+    }//GEN-LAST:event_limiterMouseClicked
+
+    private void limitItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_limitItemStateChanged
+        // TODO add your handling code here:
+        if (evt.getStateChange() == ItemEvent.SELECTED) {
+            String selected = evt.getItem().toString();
+            if ("Limit".equals(selected)) {
+                limiter.setEnabled(true);
+                limiter.setVisible(true);
+            } else {
+                limiter.setEnabled(false);
+                limiter.setVisible(false);
+            }
+            if(limit.getItemAt(1).equals("All")){
+                limiter.setEnabled(true);
+                limiter.setVisible(true);                
+            }
+        limiter.revalidate();
+        limiter.repaint();
+        }        
+    }//GEN-LAST:event_limitItemStateChanged
+
+    private void jTextField1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTextField1MouseClicked
+        // TODO add your handling code here:
+        jTextField1.setText("");      
+    }//GEN-LAST:event_jTextField1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -80,14 +261,18 @@ public class main extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new main().setVisible(true);
-            }
-        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> columnnames;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JComboBox<String> limit;
+    private javax.swing.JTextField limiter;
+    private javax.swing.JTable thetable;
     // End of variables declaration//GEN-END:variables
 }
